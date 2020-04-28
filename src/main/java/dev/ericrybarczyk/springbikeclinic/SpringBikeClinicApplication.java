c73be9ec55bba9486e2ef858eec3264fdd3afce7
Eric Rybarczyk <EricRybarczyk@gmail.com>
2020-04-28 18:43:02 -0400

    Initial Spring Boot Project Files

---
+++ src/main/java/dev/ericrybarczyk/springbikeclinic/SpringBikeClinicApplication.java
@@ -0,0 +1,13 @@
+package dev.ericrybarczyk.springbikeclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBikeClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBikeClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ericrybarczyk/springbikeclinic/SpringBikeClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ericrybarczyk.springbikeclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBikeClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
