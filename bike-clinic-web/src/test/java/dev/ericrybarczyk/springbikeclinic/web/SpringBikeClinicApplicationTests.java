879c08c8f58d01f2395fd1f5e97c51ea10e9736a
EricRybarczyk <EricRybarczyk@gmail.com>
2020-05-10 14:53:05 -0400

    Verify default unit test functions correctly

--- bike-clinic-web/src/test/java/dev/ericrybarczyk/springbikeclinic/web/SpringBikeClinicApplicationTests.java
+++ bike-clinic-web/src/test/java/dev/ericrybarczyk/springbikeclinic/web/SpringBikeClinicApplicationTests.java
@@ -3,11 +3,14 @@ package dev.ericrybarczyk.springbikeclinic.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class SpringBikeClinicApplicationTests {
 
     @Test
     void contextLoads() {
+        //fail("Force fail: making sure this test is running when I think it is...");
     }
 
 }
