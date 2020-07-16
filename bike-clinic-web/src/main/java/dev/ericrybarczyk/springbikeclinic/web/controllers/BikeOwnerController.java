b2ec64501ce56959e4671d041936b1b12fdbcd2b
EricRybarczyk <EricRybarczyk@gmail.com>
2020-07-16 11:08:26 -0400

    Added display of Bike Owners on Index page. Closes issue #15

--- bike-clinic-web/src/main/java/dev/ericrybarczyk/springbikeclinic/web/controllers/BikeOwnerController.java
+++ bike-clinic-web/src/main/java/dev/ericrybarczyk/springbikeclinic/web/controllers/BikeOwnerController.java
@@ -1,14 +1,23 @@
 package dev.ericrybarczyk.springbikeclinic.web.controllers;
 
+import dev.ericrybarczyk.springbikeclinic.services.BikeOwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/bikeOwners")
 @Controller
 public class BikeOwnerController {
 
+    private final BikeOwnerService bikeOwnerService;
+
+    public BikeOwnerController(BikeOwnerService bikeOwnerService) {
+        this.bikeOwnerService = bikeOwnerService;
+    }
+
     @RequestMapping({"", "/", "/index","/index.html"})
-    public String listBikeOwners() {
+    public String listBikeOwners(Model model) {
+        model.addAttribute("owners", bikeOwnerService.findAll());
         return "bikeOwners/index";
     }
 }
