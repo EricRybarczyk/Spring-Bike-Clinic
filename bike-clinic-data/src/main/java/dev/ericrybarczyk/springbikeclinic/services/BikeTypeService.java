eb93fa1c8a4b685f5dc91137e1d972a21b24a5ef
EricRybarczyk <EricRybarczyk@gmail.com>
2020-07-27 19:19:22 -0400

    Added BikeType Map Service Implementation. Closes issue #28

---
+++ bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/services/BikeTypeService.java
@@ -0,0 +1,7 @@
+package dev.ericrybarczyk.springbikeclinic.services;
+
+import dev.ericrybarczyk.springbikeclinic.model.BikeType;
+
+public interface BikeTypeService extends CrudService<BikeType, Long> {
+
+}
---
+++ bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/services/map/BikeTypeServiceMap.java
@@ -0,0 +1,35 @@
+package dev.ericrybarczyk.springbikeclinic.services.map;
+
+import dev.ericrybarczyk.springbikeclinic.model.BikeType;
+import dev.ericrybarczyk.springbikeclinic.services.BikeTypeService;
+import org.springframework.stereotype.Service;
+import java.util.Set;
+
+@Service
+public class BikeTypeServiceMap extends AbstractMapService<BikeType, Long> implements BikeTypeService {
+
+    @Override
+    public Set<BikeType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public BikeType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public BikeType save(BikeType bikeType) {
+        return super.save(bikeType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(BikeType bikeType) {
+        super.delete(bikeType);
+    }
+}
