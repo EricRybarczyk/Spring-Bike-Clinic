e9a39c327332b350d0f6b27edd699b09bd85f55e
EricRybarczyk <EricRybarczyk@gmail.com>
2020-07-27 19:01:31 -0400

    Added contact properties to BikeOwner entity. Closes issue #27.

--- bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/model/BikeOwner.java
+++ bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/model/BikeOwner.java
@@ -4,7 +4,43 @@ import java.util.Set;
 
 public class BikeOwner extends Person {
 
+    private String address;
+    private String city;
+    private String telephone;
+    private String emailAddress;
     private Set<Bike> bikes;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 
     public Set<Bike> getBikes() {
         return bikes;
