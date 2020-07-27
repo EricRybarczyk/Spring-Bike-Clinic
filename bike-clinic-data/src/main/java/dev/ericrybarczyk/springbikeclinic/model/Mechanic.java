320f1ea62851b163f85b7690e2d7a22cb1d651b2
EricRybarczyk <EricRybarczyk@gmail.com>
2020-07-27 18:57:40 -0400

    Created Specialty entity, added to Mechanic. Closes issue #26

--- bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/model/Mechanic.java
+++ bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/model/Mechanic.java
@@ -1,5 +1,16 @@
 package dev.ericrybarczyk.springbikeclinic.model;
 
+import java.util.Set;
+
 public class Mechanic extends Person {
 
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
