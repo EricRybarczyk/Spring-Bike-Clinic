6907e4f5d60496b16edf0e697fd9b3e39714f4e5
EricRybarczyk <EricRybarczyk@gmail.com>
2020-08-03 18:01:22 -0400

    Implemented BaseEntity as JPA MappedSuperclass. Closes issue #33

--- bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/model/BaseEntity.java
+++ bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/model/BaseEntity.java
@@ -1,8 +1,16 @@
 package dev.ericrybarczyk.springbikeclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass // mapping info applied to subclasses, but no persistence table for this class
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // Hibernate recommends the boxed type for null support
 
     public Long getId() {
