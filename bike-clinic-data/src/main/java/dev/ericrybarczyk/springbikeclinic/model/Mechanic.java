9174fe7b7f8670a34f5695e64625ca9b88ee48a7
EricRybarczyk <EricRybarczyk@gmail.com>
2020-08-03 18:25:50 -0400

    Implemented Mechanic and Specialty classes as JPA Entities. Closes issue #35

--- bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/model/Mechanic.java
+++ bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/model/Mechanic.java
@@ -1,11 +1,17 @@
 package dev.ericrybarczyk.springbikeclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "mechanics")
 public class Mechanic extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "mechanic_specialty", joinColumns = @JoinColumn(name = "mechanic_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
+
 
     public Set<Specialty> getSpecialties() {
         return specialties;
