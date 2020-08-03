202f7e93697f81a42ff748162f728b35f69e53d6
EricRybarczyk <EricRybarczyk@gmail.com>
2020-08-03 19:22:30 -0400

    Created Spring Data JPA Service for BikeOwners.Closes issue #37

--- bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/repositories/BikeOwnerRepository.java
+++ bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/repositories/BikeOwnerRepository.java
@@ -2,7 +2,8 @@ package dev.ericrybarczyk.springbikeclinic.repositories;
 
 import dev.ericrybarczyk.springbikeclinic.model.BikeOwner;
 import org.springframework.data.repository.CrudRepository;
+import java.util.Set;
 
 public interface BikeOwnerRepository extends CrudRepository<BikeOwner, Long> {
-
+    Set<BikeOwner> findByLastName(String lastName);
 }
---
+++ bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/services/jpa/BikeOwnerServiceImpl.java
@@ -0,0 +1,58 @@
+package dev.ericrybarczyk.springbikeclinic.services.jpa;
+
+import dev.ericrybarczyk.springbikeclinic.model.BikeOwner;
+import dev.ericrybarczyk.springbikeclinic.repositories.BikeOwnerRepository;
+import dev.ericrybarczyk.springbikeclinic.repositories.BikeRepository;
+import dev.ericrybarczyk.springbikeclinic.repositories.BikeTypeRepository;
+import dev.ericrybarczyk.springbikeclinic.services.BikeOwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class BikeOwnerServiceImpl implements BikeOwnerService {
+
+    private final BikeOwnerRepository bikeOwnerRepository;
+    private final BikeRepository bikeRepository;
+    private final BikeTypeRepository bikeTypeRepository;
+
+    public BikeOwnerServiceImpl(BikeOwnerRepository bikeOwnerRepository, BikeRepository bikeRepository, BikeTypeRepository bikeTypeRepository) {
+        this.bikeOwnerRepository = bikeOwnerRepository;
+        this.bikeRepository = bikeRepository;
+        this.bikeTypeRepository = bikeTypeRepository;
+    }
+
+    @Override
+    public Set<BikeOwner> findByLastName(String lastName) {
+        return new HashSet<>(bikeOwnerRepository.findByLastName(lastName));
+    }
+
+    @Override
+    public Set<BikeOwner> findAll() {
+        Set<BikeOwner> owners = new HashSet<>();
+        bikeOwnerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public BikeOwner findById(Long id) {
+        return bikeOwnerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public BikeOwner save(BikeOwner bikeOwner) {
+        return bikeOwnerRepository.save(bikeOwner);
+    }
+
+    @Override
+    public void delete(BikeOwner bikeOwner) {
+        bikeOwnerRepository.delete(bikeOwner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bikeOwnerRepository.deleteById(id);
+    }
+}
