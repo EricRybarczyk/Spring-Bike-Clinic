72b6bc81588bba2bb5ea1db34a6349078c6b2730
EricRybarczyk <EricRybarczyk@gmail.com>
2020-08-04 19:42:45 -0400

    Added missed Spring annotations

--- bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/services/jpa/BikeTypeServiceImpl.java
+++ bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/services/jpa/BikeTypeServiceImpl.java
@@ -3,9 +3,13 @@ package dev.ericrybarczyk.springbikeclinic.services.jpa;
 import dev.ericrybarczyk.springbikeclinic.model.BikeType;
 import dev.ericrybarczyk.springbikeclinic.repositories.BikeTypeRepository;
 import dev.ericrybarczyk.springbikeclinic.services.BikeTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class BikeTypeServiceImpl implements BikeTypeService {
 
     private final BikeTypeRepository bikeTypeRepository;
