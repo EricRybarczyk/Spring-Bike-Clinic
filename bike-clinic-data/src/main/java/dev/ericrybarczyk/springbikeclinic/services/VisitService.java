167d85f24b8caf6d9d44f5fd6051330219244818
EricRybarczyk <EricRybarczyk@gmail.com>
2020-08-05 20:07:03 -0400

    Created Visit Map Service. Closes issue #44

---
+++ bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package dev.ericrybarczyk.springbikeclinic.services;
+
+import dev.ericrybarczyk.springbikeclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,40 @@
+package dev.ericrybarczyk.springbikeclinic.services.map;
+
+import dev.ericrybarczyk.springbikeclinic.model.Visit;
+import dev.ericrybarczyk.springbikeclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        //make sure child objects are instantiated and are already persisted
+        if (visit.getBike() == null || visit.getBike().getOwner() == null
+                || visit.getBike().getId() == null || visit.getBike().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+}
