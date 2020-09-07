31bd83c8ceae6da62536c8dc5ba6fd616d72b615
EricRybarczyk <EricRybarczyk@gmail.com>
2020-09-07 11:23:24 -0400

    Add Lombok builder support to Specialty object

--- bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/model/Specialty.java
+++ bike-clinic-data/src/main/java/dev/ericrybarczyk/springbikeclinic/model/Specialty.java
@@ -9,10 +9,16 @@ import javax.persistence.Table;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "specialties")
 public class Specialty extends BaseEntity {
+
+    // Lombok @Builder at constructor level because this adds builder support for the ID field defined in the base class
+    @Builder
+    public Specialty(Long id, String description) {
+        super(id);
+        this.description = description;
+    }
 
     @Column(name = "description")
     private String description;
