e0b0f0c63fc69aaadc7799090578d49cac9754d4
EricRybarczyk <EricRybarczyk@gmail.com>
2020-09-07 10:03:53 -0400

    Added a simple Mechanics REST/JSON API Endpoint. Closes issue #63

--- bike-clinic-web/src/main/java/dev/ericrybarczyk/springbikeclinic/web/controllers/MechanicController.java
+++ bike-clinic-web/src/main/java/dev/ericrybarczyk/springbikeclinic/web/controllers/MechanicController.java
@@ -1,9 +1,13 @@
 package dev.ericrybarczyk.springbikeclinic.web.controllers;
 
+import dev.ericrybarczyk.springbikeclinic.model.Mechanic;
 import dev.ericrybarczyk.springbikeclinic.services.MechanicService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Set;
 
 @Controller
 public class MechanicController {
@@ -18,5 +22,11 @@ public class MechanicController {
     public String listMechanics(Model model) {
         model.addAttribute("mechanics", mechanicService.findAll());
         return "mechanics/index";
+    }
+
+    // super-simple way to get JSON data from a controller - JSON is default, XML is config option
+    @GetMapping("/api/mechanics")
+    public @ResponseBody Set<Mechanic> getMechanicsJson() {
+        return mechanicService.findAll();
     }
 }
